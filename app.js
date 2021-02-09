77f75fb7832c6ddb0c570818a0c35677e8aa390f
shing <lyshenshing20@gmail.com>
2021-02-09 13:11:38 +0700

    pack -> QR

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
+const QRCode = require('qrcode');
+
+
 const authRoute = require('./routes/auth');
+const Package = require('./data/package');
 const dbService = require('./database/dbService');
+
+// const generateQR = async text => {
+//     try {
+//         // let result = await QRCode.toFile('../images/qr.png', text);  
+//         let result = await QRCode.toFile('./images/qr.png', text);
+//         console.log(result);
+//     } catch (err) {
+//         console.log('Error -> ' + err);
+//     }
+// };
+
+// generateQR('etalket.com');
 
 app.use(express.json());
 app.use('/api/user', authRoute);
+app.use('/package', Package);
 
 
 app.listen(3000, () => {
---
+++ data/package.js
@@ -0,0 +1,36 @@
+const connection = require('../database/dbService');
+const path = require('path');
+const QRCode = require('qrcode');
+const router = require('express').Router();
+
+router.post('/addPackage', async(req, res) => {
+    const dateAdded = new Date();
+    const { shop_owner, cust_name, cust_location, cust_phone, pro_price, payment_method, service_fee, service_paid_by } = req.body;
+    try {
+        const query =  "INSERT INTO package(shop_owner, cust_name, cust_location, cust_phone, pro_price, payment_method, service_fee, service_paid_by, created_at) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        connection.query(query, [shop_owner, cust_name, cust_location, cust_phone, pro_price, payment_method, service_fee, service_paid_by, dateAdded], async(err, result) => {
+            // console.log(result);
+            if (err) {
+                console.log(err);
+                res.status(404).json({
+                    message: 'Something went wrong in our End'
+                })
+            } else {
+                const qr_id = 'QR' + result.insertId + '.png';
+                const qr_path = './images/' + qr_id;
+                // console.log(qr_path);
+                const qr_url = `https://etalket.com/?shop_owner=${shop_owner}&price=${pro_price}`;
+                await QRCode.toFile(qr_path, qr_url);
+                // res.status(200).json({
+                //     message: "Successful add package",
+                //     url: qr_url
+                // })
+                res.sendFile(path.join(__dirname, '../images', qr_id));
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = router;
