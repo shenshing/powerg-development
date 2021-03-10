47a900c8da3213bfebbdf66727fe462213541023
shing <lyshenshing20@gmail.com>
2021-03-10 19:05:51 +0700

    return package_id after added

--- data/package.js
+++ data/package.js
@@ -16,26 +16,50 @@ router.post('/addPackage', async(req, res) => {
                     message: 'Something went wrong in our End'
                 })
             } else {
-                const qr_id = 'QR' + result.insertId + '.png';
-                const qr_path = './images/' + qr_id;
-                const total_price = pro_price + service_fee;
-                // console.log(qr_path);
-                const qr_url = `https://etalket.com/?package_id=${result.insertId}&cust_name=${cust_name}&location=${cust_location}&phone_number=${cust_phone}&shop_owner=${shop_owner}&service_fee=${service_fee}&price=${pro_price}&total=${total_price}`;
+                // const qr_id = 'QR' + result.insertId + '.png';
+                // const qr_path = './images/' + qr_id;
+                // const total_price = pro_price + service_fee;
+                // const qr_url = `https://etalket.com/?package_id=${result.insertId}&cust_name=${cust_name}&location=${cust_location}&phone_number=${cust_phone}&shop_owner=${shop_owner}&service_fee=${service_fee}&price=${pro_price}&total=${total_price}`;
 
-
-
-
-                await QRCode.toFile(qr_path, qr_url);
-                // res.status(200).json({
-                //     message: "Successful add package",
-                //     url: qr_url
-                // })
-                res.sendFile(path.join(__dirname, '../images', qr_id));
+                // await QRCode.toFile(qr_path, qr_url);
+                // // res.status(200).json({
+                // //     message: "Successful add package",
+                // //     url: qr_url
+                // // })
+                // res.sendFile(path.join(__dirname, '../images', qr_id));
+                res.status(200).json({
+                    package_id: result.insertId
+                });
             }
         })
     } catch (error) {
-        console.log(error);
+        // console.log(error);
+        res.status(404).json({
+            message: error.message
+        });
     }
 });
 
+
+router.get('/getAllPackage', async(req, res) => {
+    try {
+        const query = "SELECT * FROM package;";
+        connection.query(query, async(err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(404).json({
+                    message: 'Something went wrong in our End'
+                })
+            } else {
+                res.status(200).json({
+                    data: result
+                })
+            }
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+})
 module.exports = router;
