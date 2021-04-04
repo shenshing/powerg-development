3aced2ba8fae31834ea2a295d855d51359f5f61b
lyshenshing <shing@lyshenshings-MacBook-Pro.local>
2021-04-04 21:17:08 +0700

    test db connection

--- database/dbService.js
+++ database/dbService.js
@@ -3,19 +3,48 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USERNAME,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+//     port: process.env.DB_PORT
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.log(err.message);
+//     }
+//     console.log('connected to database');
+// });
+
+const connection = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.DB_PORT
-});
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
 
-connection.connect((err) => {
+connection.getConnection((err, conn) => {
     if (err) {
-        console.log(err.message);
+        console.log('ERROR: ' + err.message);
+    } else {
+        const query = ('SELECT * FROM Users;');
+        conn.query(query, (err, result) => {
+            if(err) {
+                console.log('ERROR: ' + err.message)
+            } else {
+                // console.log(result);
+                console.log('connected to database on digitalocean')
+                // console.log(conn);
+            }
+        })
     }
-    console.log('connected to database');
+    // console.log('connected to database');
 });
 
 module.exports = connection;
