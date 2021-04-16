e674be7a96b8127dc2d8375d75125140d677017a
lyshenshing <shing.ly@pathmazing.com>
2021-04-16 13:08:13 +0700

    fix: fix bug

--- services/service.js
+++ services/service.js
@@ -11,7 +11,7 @@
             return -package.service_fee;
         }
         if (package.payment_method === 'Paid' && package.service_paid_by === 'Receiver') {
-            return package.service_fee;
+            return 0;
         }
         return 0;
     },
