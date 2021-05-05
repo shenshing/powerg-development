f9bf40de5d2d8d7371a4c189312f0e815bcae8dd
lyshenshing <shing.ly@pathmazing.com>
2021-05-05 22:35:02 +0700

    fix: update package by id

--- data/package.js
+++ data/package.js
@@ -270,7 +270,7 @@ router.delete('/deletePackageById/:id', authRole('admin'), (req, res) => {
     })
 });
 
-router.post('/updatePackageById', (req, res) => {
+router.put('/updatePackageById', (req, res) => {
     const package = req.body;
     console.log(package);
 
